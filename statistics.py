a2a208cd58938eb25568b7ce81d6536daae04f0a
vaseem205 <kvq2kor@bosch.com>
2025-07-29 16:08:58 +0530

    v2

--- statistics.py
+++ statistics.py
@@ -1,4 +1,14 @@
+import math
 
 def calculateStats(numbers):
-  # implement the computation of statistics here and return the results
-  return None
+    if not numbers:
+        return {
+            "avg": math.nan,
+            "min": math.nan,
+            "max": math.nan
+        }
+    return {
+        "avg": sum(numbers) / len(numbers),
+        "min": min(numbers),
+        "max": max(numbers)
+    }
